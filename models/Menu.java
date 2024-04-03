f34ca6e7b7dd8ff94c47139c290bf81f215ff5ae
Alexis Guzman <spartanthelexvro@gmail.com>
2024-04-03 16:50:21 -0600

    Modificantdo

--- models/Menu.java
+++ models/Menu.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-<<<<<<< HEAD
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -16,9 +15,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
-=======
+
 import javafx.scene.control.Alert;
->>>>>>> 89ba36a8f68898e729144c4c076f9d75ff70a3c2
+
 
 
 public class Menu {
